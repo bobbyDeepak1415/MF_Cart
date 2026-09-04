dec69bcda916edd6a6c11d099861c00f07d15851
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-04 12:46:24 +0530

    setup done

--- src/components/ProductsListing.jsx
+++ src/components/ProductsListing.jsx
@@ -1,4 +1,5 @@
 
+import { useState } from "react"
 import useFetchProducts from "../useFetchProducts"
 
 const url="https://dummyjson.com/products"
@@ -7,18 +8,27 @@ const url="https://dummyjson.com/products"
 const ProductsListing = () => {
 
     const {products}=useFetchProducts(url)
+    const [cartItems,setCartItems]=useState([])
+
+
+    const 
 
   return (
     <div style={{height:"100vh",width:"100vw",backgroundColor:"gray"}}>
       <h2>Products List :</h2>
 <ol>
     {products.map((product)=>{
-        return <li>{product.title}
+        return <li key={product.id}>{product.title}
         <span style={{margin:"20px",color:"green"}}>{product.price}</span>
+        <button onClick={()=>handleAddToCart(product)}>Add To Cart</button>
         </li>
     })}
 </ol>
-      {}
+      
+      <div>
+        <h3>Your Cart Here</h3>
+        {cartItems}
+      </div>
 
     </div>
   )
